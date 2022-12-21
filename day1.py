5663c5fddb8bb0c54e32ced160ad9b6da39487a3
Tom Furlong <48124393+tomfurlong@users.noreply.github.com>
2022-12-21 19:43:08 +0000

    Day 1 part 1 Submitted and Correct

--- day1.py
+++ day1.py
@@ -1,2 +1,23 @@
+largest = 0
+next = 0
 with open('input.txt') as f:
     lines = f.readlines()
+
+group_count = 1
+for line in lines:
+    if line == '\n':
+        group_count+=1
+
+for line in lines:
+    if line == '\n':
+        print(next)
+        if next>largest:
+            print('New largest: ', next, ' Prev largest: ', largest)
+            largest = next
+        next = 0
+        continue 
+    next += int(line)
+    print('largest: ' ,largest, ' next: ', next)
+    
+    
+print(largest)
